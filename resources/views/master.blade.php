dd751293ede1b6c0303289b48af5c6609a9b3a54
Mateus Guimarães <mateus.jatene@gmail.com>
2016-01-29 17:19:15 -0300

    Base template

---
+++ resources/views/master.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Coalition test</title>
+
+		<!-- Bootstrap CSS -->
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		    <div class="container">
+
+
+		      <div class="jumbotron">
+		        <h1>Coalition test</h1>
+		        <p class="lead">You may add stuff in the form below.</p>
+		        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+		      </div>
+
+		      <div class="row marketing">
+		        <div class="col-lg-6">
+		          <h4>Subheading</h4>
+		          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+		          <h4>Subheading</h4>
+		          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+		          <h4>Subheading</h4>
+		          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+		        </div>
+
+		        <div class="col-lg-6">
+		          <h4>Subheading</h4>
+		          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+		          <h4>Subheading</h4>
+		          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+		          <h4>Subheading</h4>
+		          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+		        </div>
+		      </div>
+
+		      <footer class="footer">
+		        <p>&copy; 2015 Company, Inc.</p>
+		      </footer>
+
+		    </div> <!-- /container -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</body>
+</html>
