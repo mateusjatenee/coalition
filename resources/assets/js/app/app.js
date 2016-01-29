922341b477dfc0a5939fdf8b90291acee912e333
Mateus Guimarães <mateus.jatene@gmail.com>
2016-01-29 17:29:23 -0300

    Minor vue config

--- resources/assets/js/app/app.js
+++ resources/assets/js/app/app.js
@@ -3,8 +3,14 @@ var Vue = require('vue');
 Vue.use(require('vue-resource'));
 Vue.http.headers.common['X-CSRF-TOKEN'] = $('meta[name="csrf-token"]').attr('content');
 
+import Products from './components/Products.vue';
 
 new Vue({
 	el: 'body',
+
+	components: {
+		Products: Products
+	}
+
 
 });
---
+++ resources/assets/js/app/components/Products.vue
@@ -0,0 +1,33 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			productData: {
+				name: null,
+				qty: null,
+				price: null
+			},
+			loading: false
+		}
+	},
+
+	methods: {
+		sendForm() {
+			var resource = this.$resource('api/products');
+			this.loading: true;
+			resource.save({product: this.productData}).then(function(response) {
+				this.loading = false;
+			}, function(response) {
+				console.log(response);
+				this.loading = false;
+			});
+		}
+	}
+}
+
+</script>
