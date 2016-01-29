9f43ed352469b52b71c77b41498d91df535d39bf
Mateus Guimarães <mateus.jatene@gmail.com>
2016-01-29 17:41:33 -0300

    API + routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,3 +29,7 @@ Route::get('/', function () {
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+Route::group(['middleware' => ['api'], 'prefix' => 'api', 'namespace' => 'Api'], function () {
+    Route::resource('products', 'ProductsController');
+});
