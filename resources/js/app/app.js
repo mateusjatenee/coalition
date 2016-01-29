e5f4e4c8551c335831ad58f3b67de58f2a62fb4a
Mateus Guimarães <mateus.jatene@gmail.com>
2016-01-29 17:22:55 -0300

    adds vue.js

---
+++ resources/js/app/app.js
@@ -0,0 +1,10 @@
+var Vue = require('vue');
+
+Vue.use(require('vue-resource'));
+Vue.http.headers.common['X-CSRF-TOKEN'] = $('meta[name="csrf-token"]').attr('content');
+
+
+new Vue({
+	el: 'body',
+
+});
